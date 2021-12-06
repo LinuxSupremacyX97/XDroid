ab4b2290eba6f53fa23f2a3b786a2b419c559f0c
MatrixMenXD <87778892+MatrixMenXD@users.noreply.github.com>
2021-12-06 16:44:37 +0530

    Update creds.sh

--- creds.sh
+++ creds.sh
@@ -2,7 +2,7 @@
 
 username="MatrixMenXD" # your username of github account
 repo="CyborgXDroid" # your fork or cloned repo's name
-upstream="https://github.com/ashwinstr/UX-justu.git" # the upstream repo link from which the commits will be pulled
+upstream="https://github.com/ashwinstr/UX-jutsu.git" # the upstream repo link from which the commits will be pulled
 
 echo "###"
 echo "Username - ${username}"
